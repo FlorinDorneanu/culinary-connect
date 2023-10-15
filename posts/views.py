047bcc55b28ed7fcfa0665dfc67a2a3c16ea6adb
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-15 16:17:11 +0000

    Build: Add PostSerializer fields

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
+from django.db.models import Count
 
 
 class PostList(generics.ListCreateAPIView):
