cbc2894e903c51d043eeb63fa7ba9e252d16fe8a
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-15 14:29:46 +0000

    Build: Add image filter field to posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -17,5 +17,6 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-            'title', 'content', 'image'
+            'title', 'content', 'image', 'image_filter'
         ]
+
