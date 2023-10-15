91d3b4d823494fa41a1d905e3132112521af4415
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-15 16:23:01 +0000

    Build: Add fields to filter for PostList and PostDetail views

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
@@ -16,6 +16,12 @@ class PostList(generics.ListCreateAPIView):
         comments_count=Count('comment', distinct=True)
         likes_count=Count('likes', distinct=True)
     ).order_by('-created_at')
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = [
+        'likes_count', 
+        'comments_count', 
+        'likes__created_at'
+    ]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -27,4 +33,7 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Post.objects.all()
+    queryset = Post.objects.annotate(
+        comments_count=Count('comment', distinct=True)
+        likes_count=Count('likes', distinct=True)
+    ).order_by('-created_at')
