c556953ae929272efa1a0d72c4e85743577f2db8
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-17 20:07:28 +0000

    Build: Add search feature for events

--- events/views.py
+++ events/views.py
@@ -1,20 +1,41 @@
 
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, filters
 from .models import Event
 from .serializers import EventSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class EventList(generics.ListCreateAPIView):
+    """
+    Retrieve events from DB.
+    Create new events.
+    """
     serializer_class = EventSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Event.objects.all()
 
     def perform_create(self, serializer):
+        """
+        Check for user authentication.
+        """
         serializer.save(owner=self.request.user)
+
+    filter_backends = [
+        filters.SearchFilter,
+    ]
+
+    search_fields = [
+        'owner__username',
+        'title',
+        'city',
+        'country',
+    ]
 
 
 class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+     """
+    Retrieve, Update & Destroy events.
+    """
     serializer_class = EventSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Event.objects.all()
