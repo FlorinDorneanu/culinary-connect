4cfb5ae9a74de4b1167b57844ee4f214f10942b3
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-22 19:11:34 +0000

    Add: date and time formatting

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -89,6 +89,7 @@ REST_FRAMEWORK = {
     if 'DEV' not in os.environ:
         REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
           'restframework.renderers.JSONRenderer']
+    'DATETIME_FORMAT': '%d %b %Y'
 }
 
 MIDDLEWARE = [
