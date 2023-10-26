fcde017e2fa9846d60ae906535c7c019a6ba0e4b
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-26 14:57:03 +0000

    Build: Handle routing and add css to navbar.js and app.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,30 +1,46 @@
-import React from 'react';
-import { Navbar, Container, Nav } from 'react-bootstrap';
-import logo from '../assets/logo.png';
-import styles from '../styles/NavBar.module.css';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
-        <Container>
-            <Navbar.Brand >
-                <img src ={logo} alt='logo' height='60' />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto text-right">
-                    <Nav.Link>
-                        <i class="fa-solid fa-umbrella-beach"></i>Home
-                    </Nav.Link>
-                    <Nav.Link>
-                        <i class="fa-solid fa-arrow-right-to-bracket"></i>Sign in
-                    </Nav.Link>
-                    <Nav.Link>
-                        <i class="fa-solid fa-user-plus"></i>Sign up
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fa-solid fa-umbrella-beach"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fa-solid fa-arrow-right-to-bracket"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fa-solid fa-user-plus"></i>Sign up
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
