0bbcc45d797e8fc2c7ca0f2eb7fa98f15145b75e
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-28 16:35:11 +0000

    Add: Tabs for Posts and Events to ProfilePage

--- frontend/src/pages/profiles/ProfilePage.js
+++ frontend/src/pages/profiles/ProfilePage.js
@@ -5,6 +5,8 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import Asset from "../../components/Asset";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 import styles from "../../styles/ProfilePage.module.css";
 import appStyles from "../../App.module.css";
@@ -21,6 +23,7 @@ import {
 import { Button, Image } from "react-bootstrap";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../posts/Post";
+import Event from "../events/Event";
 import { fetchMoreData } from "../../utils/utils";
 import NoResults from "../../assets/no-results.png";
 import { ProfileEditDropdown } from "../../components/MoreDropdown";
@@ -28,6 +31,7 @@ import { ProfileEditDropdown } from "../../components/MoreDropdown";
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const [profilePosts, setProfilePosts] = useState({ results: [] });
+  const [profileEvents, setProfileEvents] = useState({ results: [] });
 
   const currentUser = useCurrentUser();
   const { id } = useParams();
@@ -41,16 +45,18 @@ function ProfilePage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [{ data: pageProfile }, { data: profilePosts }] =
+        const [{ data: pageProfile }, { data: profilePosts },{ data: profileEvents }] =
           await Promise.all([
             axiosReq.get(`/profiles/${id}/`),
             axiosReq.get(`/posts/?owner__profile=${id}`),
+            axiosReq.get(`/events/?owner__profile=${id}`),
           ]);
         setProfileData((prevState) => ({
           ...prevState,
           pageProfile: { results: [pageProfile] },
         }));
         setProfilePosts(profilePosts);
+        setProfileEvents(profileEvents);
         setHasLoaded(true);
       } catch (err) {
         console.log(err);
@@ -76,6 +82,10 @@ function ProfilePage() {
             <Col xs={3} className="my-2">
               <div>{profile?.posts_count}</div>
               <div>posts</div>
+            </Col>
+            <Col xs={3} className="my-2">
+              <div>{profile?.events_count}</div>
+              <div>events</div>
             </Col>
             <Col xs={3} className="my-2">
               <div>{profile?.followers_count}</div>
@@ -135,6 +145,41 @@ function ProfilePage() {
     </>
   );
 
+  const mainProfileEvents = (
+    <>
+      {profileEvents.results.length ? (
+        <InfiniteScroll
+          children={profileEvents.results.map((event) => (
+            <Event key={event.id} {...event} setEvents={setProfileEvents} />
+          ))}
+          dataLength={profileEvents.results.length}
+          loader={<Asset spinner />}
+          hasMore={!!profileEvents.next}
+          next={() => fetchMoreData(profileEvents, setProfileEvents)}
+        />
+      ) : (
+        <Asset
+          src={NoResults}
+          message={`No results found, ${profile?.owner} hasn't created any events yet.`}
+        />
+      )}
+    </>
+  );
+
+  const profileTabs = (
+    <>
+      <Tabs defaultActiveKey="posts" id="profile-tab" className={styles.Tabs}>
+        <Tab eventKey="posts" title={`${profile?.owner}'s posts`} className={styles.Tab}>
+          {mainProfilePosts}
+        </Tab>
+        <Tab eventKey="events" title={`${profile?.owner}'s events`} className={styles.Tab}>
+          {mainProfileEvents}
+        </Tab>
+      </Tabs>
+    </>
+  );
+
+
   return (
     <Row>
       <Col className="py-2 p-0 p-lg-2" lg={8}>
@@ -143,7 +188,7 @@ function ProfilePage() {
           {hasLoaded ? (
             <>
               {mainProfile}
-              {mainProfilePosts}
+              {profileTabs}
             </>
           ) : (
             <Asset spinner />
