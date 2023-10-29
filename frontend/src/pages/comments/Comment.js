c37ff4df3c4845bc52abd48b49d8324fe55fa70b
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-29 16:16:32 +0000

    Add handleLike and handleunlike functionality to comments

--- frontend/src/pages/comments/Comment.js
+++ frontend/src/pages/comments/Comment.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Media } from "react-bootstrap";
+import {OverlayTrigger, Tooltip, Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
 import { MoreDropdown } from "../../components/MoreDropdown";
@@ -16,6 +16,8 @@ const Comment = (props) => {
     owner,
     updated_at,
     content,
+    likecomment_count,
+    likecomment_id,
     id,
     setPost,
     setComments,
@@ -42,6 +44,38 @@ const Comment = (props) => {
         results: prevComments.results.filter((comment) => comment.id !== id),
       }));
     } catch (err) {}
+  };
+
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likecomment/", { comment: id });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? { ...comment, likecomment_count: comment.likecomment_count + 1, likecomment_id: data.id }
+            : comment;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnlike = async () => {
+    try {
+      await axiosRes.delete(`/likecomment/${likecomment_id}/`);
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? { ...comment, likecomment_count: comment.likecomment_count - 1, likecomment_id: null }
+            : comment;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -74,6 +108,32 @@ const Comment = (props) => {
           />
         )}
       </Media>
+        <div className={styles.CommentBar}>
+          {is_owner ? (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You can't like your own Comment!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          ) : likecomment_id ? (
+            <span onClick={handleUnlike}>
+              <i className={`fas fa-heart ${styles.Heart}`} />
+            </span>
+          ) : currentUser ? (
+            <span onClick={handleLike}>
+              <i className={`far fa-heart ${styles.HeartOutline}`} />
+            </span>
+          ) : (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>Log in to like Comments!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          )}
+          {likecomment_count}
+        </div>
     </>
   );
 };
