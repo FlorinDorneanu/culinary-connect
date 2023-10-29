ee012d73a83ef9dee3ab964b749893ca94f181d3
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-29 15:29:18 +0000

    Build: LikeCommentList, LikeCommentDetail views and create urls paths

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('', include('posts.urls')),
     path('', include('comments.urls')),
     path('', include('likes.urls')),
+    path('', include('likecomment.urls')),
     path('', include('followers.urls')),
     path('', include('events.urls')),
 ]
--- likecomment/views.py
+++ likecomment/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likecomment.models import LikeComment
+from likecomment.serializers import LikeCommentSerializer
 
-# Create your views here.
+
+class LikeCommentList(generics.ListCreateAPIView):
+    """
+    List comment likes or create a comment like if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeCommentSerializer
+    queryset = LikeComment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class LikeCommentDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a comment like or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeCommentSerializer
+    queryset = LikeComment.objects.all()
