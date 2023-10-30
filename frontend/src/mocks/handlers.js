e9869ebeb2e74f6878cfefd6d83bd6b3b25144c4
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-10-30 09:30:18 +0000

    Testing: Add jest testing

---
+++ frontend/src/mocks/handlers.js
@@ -0,0 +1,22 @@
+import { rest } from "msw";
+
+const baseURL = "/api";
+
+export const handlers = [
+  rest.get(`${baseURL}/dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 2,
+        username: "admin",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 2,
+        profile_image: "https://res.cloudinary.com/florindorneanu/image/upload/v1/media/../default_profile_btl0op"
+      })
+    );
+  }),
+  rest.post(`${baseURL}/dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
